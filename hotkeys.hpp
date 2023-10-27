51e2cb2da659a5e1510174b6e7fd31040a508cfd
nikinov <novellenicholas@gmail.com>
2023-10-27 17:42:27 +0200

    Update hotkeys.hpp

--- hotkeys.hpp
+++ hotkeys.hpp
@@ -1,8 +1,17 @@
 #pragma once
 #include "t_keys.hpp"
 
+#include <functional>
+
 class HotKeys: TKeys
 {
 public:
-    
+    std::map<KeyCodes, std::string> label_map;
+    void start_recording();
+private:
+    std::function<void(const std::vector<int> &)> callback_;
+    std::vector<int> keyStates_;
+    std::thread recordingThread_;
+    bool isRecording_ = false;
+    void recording_thread();
 };
